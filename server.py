c40955e85a9144a4964f5b2e25dbf924c463aa38
Jeremy0818 <sheeyeowchoo@gmail.com>
2021-01-28 10:03:25 -0700

    redirect functionality fixed, link to directory instead of index.html

--- server.py
+++ server.py
@@ -52,8 +52,8 @@ class HTTPResponse():
 	def header(self):
 		h = self.httpVersion + " " + self.code + " " + self.statusText[self.code] + " \r\n" +\
 				"Content-Type: " + self.ContentType + "\r\n"
-		# if len(self.Location) > 0:
-		# 	h += "Location: " + self.Location + "\r\n"
+		if len(self.Location) > 0:
+			h += "Location: " + self.Location + "\r\n"
 		h+= "\r\n"
 		print("----- Header -----")
 		print(h)
@@ -183,7 +183,7 @@ class HTTPResponse():
 			self.code = "301"
 			d = self.URL.split('/')[-1]
 			print("directory: ", d)
-			self.Location = d + "/index.html"
+			self.Location = d + "/"
 			self.body = self.header() + self.get301(d + "/index.html")
 			return
 		except FileNotFoundError:
